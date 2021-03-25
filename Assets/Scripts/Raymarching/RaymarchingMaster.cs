889fd7990d4781fc81b455f6168015d6d80cbd71
humblefool_01 <kaushikvishwanath010@gmail.com>
2021-03-24 22:22:08 -0700

    Initial commit

---
+++ Assets/Scripts/Raymarching/RaymarchingMaster.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaymarchingMaster : MonoBehaviour
+{
+    public ComputeShader RaymarchingShader;
+    public Texture SkyboxTexture;
+    public Light DirectionalLight;
+    private RenderTexture _target;
+    private ComputeBuffer _sphereBuffer;
+
+    private Camera _camera;
+    [SerializeField]
+    private GameObject ground;
+    [SerializeField]
+    private GameObject sphere;
+    [SerializeField]
+    private float sphereRadius;
+    [SerializeField]
+    private GameObject box;
+    [SerializeField]
+    private GameObject prism;
+    [SerializeField]
+    private GameObject torus;
+    [Range(1, 10)]
+    public int numberOfReflections = 8;
+    [SerializeField]
+    private bool smoothBlend = false;
+    [Range(0.01f, 1.0f)]
+    [SerializeField]
+    private float blendCoefficient = 0.01f;
+    [SerializeField]
+    private Color color;
+
+    struct Shape
+    {
+        Vector3 position;
+        Vector3 size;
+        Vector3 colour;
+        Vector3 albedo;
+    };
+
+    private void OnEnable()
+    {
+        //color = Random.ColorHSV();
+    }
+    private void OnDisable()
+    {
+
+    }
+
+    private void Awake()
+    {
+        _camera = this.GetComponent<Camera>();
+    }
+
+    private void SetShaderParameters()
+    {
+        RaymarchingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);   // _CameraToWorld declared in compute shader
+        RaymarchingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse); // _CameraInverseProjection declared in compute shader
+        RaymarchingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);
+        Vector3 l = DirectionalLight.transform.forward;
+        RaymarchingShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
+        RaymarchingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
+        RaymarchingShader.SetVector("_Albedo", new Vector3(color.r, color.g, color.b));
+        RaymarchingShader.SetInt("_NumberOfReflections", numberOfReflections);
+        RaymarchingShader.SetBool("_SmoothBlend", smoothBlend);
+        RaymarchingShader.SetFloat("_BlendCoefficient", blendCoefficient);
+        RaymarchingShader.SetVector("_Ground", ground.transform.position);
+        RaymarchingShader.SetVector("_GroundScale", ground.transform.localScale);
+        RaymarchingShader.SetVector("_Sphere", new Vector4(sphere.transform.position.x, sphere.transform.position.y, sphere.transform.position.z, sphereRadius));
+        RaymarchingShader.SetVector("_Box", box.transform.position);
+        RaymarchingShader.SetVector("_BoxScale", box.transform.localScale);
+        RaymarchingShader.SetVector("_Prism", prism.transform.position);
+        RaymarchingShader.SetVector("_PrismSize", new Vector2(prism.transform.localScale.x, prism.transform.localScale.y));
+        RaymarchingShader.SetVector("_Torus", torus.transform.position);
+        RaymarchingShader.SetVector("_TorusSize", new Vector2(torus.transform.localScale.x, torus.transform.localScale.y));
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        //_camera = Camera.current;
+        //DirectionalLight = FindObjectOfType<Light>();
+
+        SetShaderParameters();
+        Render(destination);
+    }
+
+    private void InitRenderTexture()
+    {
+        if (_target == null || _target.width != Screen.width || _target.height != Screen.height)
+        {
+            // Release render texture if we already have one
+            if (_target != null)
+                _target.Release();
+
+            // Get a render target for Ray Tracing
+            _target = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _target.enableRandomWrite = true;
+            _target.Create();
+        }
+    }
+
+    private void Render(RenderTexture destination)
+    {
+        // Make sure we have a current render target
+        InitRenderTexture();
+
+        // Set the target and dispatch the compute shader
+        RaymarchingShader.SetTexture(0, "Result", _target);
+
+        /* 
+        Each thread group consists of a number of threads which is set in the shader itself. 
+        The size and number of thread groups can be specified in up to three dimensions, which makes it easy to apply compute shaders to problems of either dimensionality. 
+        In our case, we want to spawn one thread per pixel of the render target.
+        The default thread group size as defined in the Unity compute shader template is [numthreads(8,8,1)]
+        */
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
+        RaymarchingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+
+        // Finally, we write our result to the screen using Graphics.Blit.
+        Graphics.Blit(_target, destination);
+    }
+}
---
+++ Assets/Scripts/Raytracing/Raytracing.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raytracing : MonoBehaviour
+{
+    public ComputeShader RaytracngShader;
+    public Texture SkyboxTexture;
+    public Light DirectionalLight;
+    private RenderTexture _target;
+
+    private Camera _camera;
+    public Vector2 SphereRadius = new Vector2(3.0f, 8.0f);
+    public uint SpheresMax = 100;
+    public float SpherePlacementRadius = 100.0f;
+    private ComputeBuffer _sphereBuffer;
+
+    private int _currentSample = 0;
+    private Material _addMaterial;
+    struct Sphere
+    {
+        public Vector3 position;
+        public float radius;
+        public Vector3 albedo;
+        public Vector3 specular;
+    };
+
+    private void OnEnable()
+    {
+        _currentSample = 0;
+        SetUpScene();
+    }
+
+    private void OnDisable()
+    {
+        if (_sphereBuffer != null)
+            _sphereBuffer.Release();
+    }
+
+    private void Awake()
+    {
+        _camera = this.GetComponent<Camera>();
+    }
+
+    private void SetUpScene()
+    {
+        List<Sphere> spheres = new List<Sphere>();
+        for (int i = 0; i < SpheresMax; i++)
+        {
+            Sphere sphere = new Sphere();
+            sphere.radius = SphereRadius.x + Random.value * (SphereRadius.y - SphereRadius.x);
+            Vector2 randomPos = Random.insideUnitCircle * SpherePlacementRadius;
+            sphere.position = new Vector3(randomPos.x, sphere.radius, randomPos.y);
+
+            // Reject spheres that are intersecting others
+            foreach (Sphere other in spheres)
+            {
+                float minDist = sphere.radius + other.radius;
+                if (Vector3.SqrMagnitude(sphere.position - other.position) < minDist * minDist)
+                    goto SkipSphere;
+            }
+
+            // Albedo and specular color
+            Color color = Random.ColorHSV();
+            bool metal = Random.value < 0.5f;
+            sphere.albedo = metal ? Vector3.zero : new Vector3(color.r, color.g, color.b);
+            sphere.specular = metal ? new Vector3(color.r, color.g, color.b) : Vector3.one * 0.04f;
+
+            // Add the sphere to the list
+            spheres.Add(sphere);
+
+        SkipSphere:
+                    continue;
+        }
+
+        // Assign to compute buffer
+        _sphereBuffer = new ComputeBuffer(spheres.Count, 40);
+        _sphereBuffer.SetData(spheres);
+        // The magic number 40 in new ComputeBuffer(spheres.Count, 40) is the stride of our buffer, i.e. the byte size of one sphere in memory.
+
+        RaytracngShader.SetBuffer(0, "_Spheres", _sphereBuffer);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        SetShaderParameters();
+        Render(destination);
+    }
+
+    // Call SetShaderParameters from OnRenderImage before rendering.
+    private void SetShaderParameters()
+    {
+        RaytracngShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);   // _CameraToWorld declared in compute shader
+        RaytracngShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse); // _CameraInverseProjection declared in compute shader
+        RaytracngShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);
+        Vector3 l = DirectionalLight.transform.forward;
+        RaytracngShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
+        RaytracngShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
+    }
+
+    private void Render(RenderTexture destination)
+    {
+        // Make sure we have a current render target
+        InitRenderTexture();
+
+        // Set the target and dispatch the compute shader
+        RaytracngShader.SetTexture(0, "Result", _target);
+
+        /* 
+        Each thread group consists of a number of threads which is set in the shader itself. 
+        The size and number of thread groups can be specified in up to three dimensions, which makes it easy to apply compute shaders to problems of either dimensionality. 
+        In our case, we want to spawn one thread per pixel of the render target.
+        The default thread group size as defined in the Unity compute shader template is [numthreads(8,8,1)]
+        */
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
+        RaytracngShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+
+        // Finally, we write our result to the screen using Graphics.Blit.
+        if (_addMaterial == null)
+        {
+            _addMaterial = new Material(Shader.Find("Hidden/AddShader"));
+        }
+        _addMaterial.SetFloat("_Sample", _currentSample);
+        Graphics.Blit(_target, destination, _addMaterial);
+        _currentSample += 1;
+    }
+
+    private void InitRenderTexture()
+    {
+        if (_target == null || _target.width != Screen.width || _target.height != Screen.height)
+        {
+            // Release render texture if we already have one
+            if (_target != null)
+                _target.Release();
+
+            // Get a render target for Ray Tracing
+            _currentSample = 0;
+            _target = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _target.enableRandomWrite = true;
+            _target.Create();
+        }
+    }
+
+    private void Update()
+    {
+        if (transform.hasChanged)
+        {
+            _currentSample = 0;
+            transform.hasChanged = false;
+        }
+    }
+}
